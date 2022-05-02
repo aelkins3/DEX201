975da969fd250166ca8bfde2e98087b611908000
Andrew Elkins <Andrew.j.elkins@gmail.com>
2022-05-02 12:38:41 -0400

    F

--- contracts/wallet.sol
+++ contracts/wallet.sol
@@ -1,3 +1,5 @@
+//SPDX-License-Identifier: MIT
+
 pragma solidity >=0.6.0 <0.8.13;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
--- test/wallettest.js
+++ test/wallettest.js
@@ -1,13 +1,37 @@
-const Dex = artifacts.require("Dex")
-const Link = artifacts.require("Link")
+//SPDX-License-Identifier: MIT
+
+const Dex = artifacts.require('Dex');
+const Link = artifacts.require('Link');
 const truffleAssert = require('truffle-assertions');
 
-contract("Dex", accounts => {
-    it("should only be possible for owner to add tokens", async () => {
-        let dex = await Dex.deployed()
-        let link = await Link.deployed()
-        await truffleAssert.passes(
-            dex.addToken(web3.utils.fromUtf8("LINK"), link.address, {from: accounts[0]})
-        )
-    })
-})
+contract('Dex', (accounts) => {
+  let linkInstance;
+  let dexInstance;
+
+  //set contracts instances
+  before(async function() {
+    // Deploy tokens to testnet
+    linkInstance = await Link.new();
+    dexInstance = await Dex.new();
+  });
+
+  describe('Dex', () => {
+    it('should only be possible for owner to add tokens', async () => {
+      //   let dex = await Dex.deployed();
+      //   let link = await Link.deployed();
+      let dex = dexInstance;
+      let link = linkInstance;
+
+      await truffleAssert.passes(
+        dex.addToken(web3.utils.fromUtf8('LINK'), link.address, {
+          from: accounts[0],
+        })
+      );
+      //   await truffleAssert.passes(
+      //     dex.addToken(web3.utils.fromUtf8('LINK'), link.address, {
+      //       from: accounts[1],
+      //     })
+      //   );
+    });
+  });
+});
